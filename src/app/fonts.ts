b2a5c7fa08ea1042c22fd508eff841f49731da57
Andres Artunduaga <andres.artunduaga@outlook.com>
2025-01-29 17:05:41 -0500

    Hello World!

---
+++ src/app/fonts.ts
@@ -0,0 +1,16 @@
+import { Inter, Roboto_Mono, Audiowide } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const roboto_mono = Roboto_Mono({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const audiowide = Audiowide({
+  subsets: ["latin"],
+  weight: "400",
+});
